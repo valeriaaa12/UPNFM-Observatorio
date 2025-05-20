92e93d8b4a46f0014980e91a656e82bd01208713
Akeem Ieong Ham <akeemieong@gmail.com>
2025-05-20 13:37:17 -0600

    modificacion del pdf

--- src/pages/Boletines.tsx
+++ src/pages/Boletines.tsx
@@ -3,16 +3,17 @@ export default function Boletines() {
   return (
     <>
        <div>
+        <h2>Boletines</h2>
         <div className="card" style={{width: '18rem'}}>
-          <embed 
-            src="/Boletin_1_Observatorio_educativo_UPNFM.pdf" 
-            type="application/pdf" 
-            width="100%" 
-            height="200px" 
-          />
+          <img src="/images/boletin 1.png" 
+               className="card-img-top" 
+               alt="..." 
+               width="100" 
+               height="300"/>
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <a href="/Boletin_1_Observatorio_educativo_UPNFM.pdf" className="btn btn-primary">Go somewhere</a>
+            <a href="#" className="btn btn-primary">texto 2</a>
           </div>
         </div>
        </div>
