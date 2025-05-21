9161841b11871bd7264b6e88a4ddf518395a6580
Akeem Ieong Ham <akeemieong@gmail.com>
2025-05-21 10:06:12 -0600

    Avance de boletines

--- src/pages/Boletines.tsx
+++ src/pages/Boletines.tsx
@@ -1,21 +1,47 @@
-
+import Boletin from "@/cards/Boletin"
 export default function Boletines() {
   return (
     <>
-       <div>
-        <h2>Boletines</h2>
-        <div className="card" style={{width: '18rem'}}>
-          <img src="/images/boletin 1.png" 
-               className="card-img-top" 
-               alt="..." 
-               width="100" 
-               height="300"/>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <a href="/Boletin_1_Observatorio_educativo_UPNFM.pdf" className="btn btn-primary">Go somewhere</a>
-            <a href="#" className="btn btn-primary">texto 2</a>
-          </div>
+       <div className="font">
+        <div className="container-boletin">
+          <h2 >Boletines</h2>
         </div>
+          <div id="Boletines" className="card-gallery">
+            <Boletin 
+              title= "Boletin 1"
+              pdf= "/Boletin_1_Observatorio_educativo_UPNFM.pdf"
+            />
+            <Boletin 
+              title= "Boletin 2"
+              pdf= "/Boletin_2_Observatorio_educativo_UPNFM.pdf"
+            />
+            <Boletin 
+              title= "Boletin 3"
+              
+              pdf= "/Boletin_3_Observatorio_educativo_UPNFM.pdf"
+            />
+            <Boletin 
+              title= "Boletin 4"
+              pdf= "/Boletin_4_Observatorio_educativo_UPNFM.pdf"
+            />
+            <Boletin 
+              title= "Boletin 5"
+              pdf= "/Boletín_No_5_Observatorio_Universitario_de_la_Educacion_Nacional_Internacional_UPNFM.pdf"
+            />
+            <Boletin 
+              title= "Boletin 6"
+              pdf= "/Boletín_No_6_Observatorio_Universitario_de_la_Educacion_Nacional_Internacional_UPNFM.pdf"
+            />
+            <Boletin 
+              title= "Boletin 7"
+              pdf= "/Boletín_No_7_Observatorio_Universitario_de_la_Educacion_Nacional_Internacional_UPNFM.pdf"
+            />
+            <Boletin 
+              title= "Boletin 8"
+              pdf= "/Boletín_No_8_Observatorio_Universitario_de_la_Educacion_Nacional_Internacional_UPNFM.pdf"
+            />
+
+          </div>
        </div>
     </>
     );
