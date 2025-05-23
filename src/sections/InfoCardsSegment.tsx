1f500216741dd8e922c61708f9b32001fdcebec2
Andrea <andreaortez11@gmail.com>
2025-05-23 03:16:03 -0600

    creacion de portadas y conexiones de las paginas

--- src/sections/InfoCardsSegment.tsx
+++ src/sections/InfoCardsSegment.tsx
@@ -1,5 +1,4 @@
 import InfoCard from "@/pages/cards/InfoCard";
-import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
