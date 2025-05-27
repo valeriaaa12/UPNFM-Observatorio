2adae4b2dc1eca5b6eb3cf3c8997070aa53e69fa
Dariel Sevilla <darielsevilla@gmail.com>
2025-05-27 02:29:14 -0600

    power bi added

---
+++ src/maps/HondurasMap.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useRef } from 'react';
+import * as pbi from 'powerbi-client';
+
+
+
+export default function HondurasMap(){
+   return(
+     <iframe title="prueba" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiOTAyNTY2MjctYTYxMS00NTQzLWI2MGItYzM3NjExOWUwODQzIiwidCI6ImFmMmZkMTk2LTFkOWYtNDdiNC05MDY5LTM5MWE0NmY4MzYwMSIsImMiOjR9&filterPaneEnabled=false&navContentPaneEnabled=false&filterPaneEnabled=false&navContentPaneEnabled=false&actionBarEnabled=false" frameBorder="0" allowFullScreen={true}></iframe>
+   );
+    };
