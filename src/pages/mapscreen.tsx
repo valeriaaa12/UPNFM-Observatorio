1c25a72f01f123cbd96ba6873f987cb8f609d299
Dariel Sevilla <darielsevilla@gmail.com>
2025-05-29 14:35:26 -0600

    optimized map

--- src/pages/mapscreen.tsx
+++ src/pages/mapscreen.tsx
@@ -2,17 +2,82 @@
 import NavBar from "@/navigation/NavBar";
 import dynamic from 'next/dynamic';
 import ComboBox from "@/components/combobox";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import MapFilters from "@/sections/mapfilters";
+import axios from 'axios'
 
 const MainMap = dynamic(() => import("@/maps/MainMap"), {
   ssr: false
 });
 
+//mapeo de datos
+interface department{
+  name: string;
+  legend: string;
+  value: number;
+  year: string;
+  level: string;
+}
+
+
+
 export default function MapScreen() {
   const [selectedYear, setSelectedYear] = useState("2024");
   const [level, setLevel] = useState("Básica III Ciclo")
-  const years = ['Todos', '2020', '2021', '2022', '2023', '2024'];
+  const [departments, setDepartments] = useState<department[] | null>(null);
+  const [filteredDepartments, setFilteredDepartments] = useState<department[] | null>(null);
 
+  {/*mapeo*/}
+  //metodo de mapeo
+  const mapData = async () =>{
+    console.log("start")
+    try{
+      const config = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      }  
+
+      
+
+      const url = process.env.NEXT_PUBLIC_API_URL + "/desercion"
+
+      const response = await axios.get(url, config)
+      
+      const tempoDepartments: department[] = response.data.map((item: any)=>({
+        name: item.departamento.toLowerCase(),
+        legend: item.leyenda,
+        value: item.tasa_desercion,
+        year: item.periodo_anual,
+        level: item.nivel
+      }))
+
+      console.log(response)
+      await setDepartments(tempoDepartments)
+
+      filterData();
+      }catch(error : unknown){
+        console.log(error)
+        console.log("end")
+      }
+  }
+
+  const filterData = async () =>{
+     const filtered = departments?.filter(
+      (item) => item.year == selectedYear && item.level == level
+    )?? null;
+      await setFilteredDepartments(filtered)
+      console.log(filtered)
+  }
+
+  //useStated necesarios
+  useEffect(()=>{
+    mapData()
+  }, [])
+
+  useEffect(()=>{
+    filterData()
+  }, [selectedYear, level])
   return (
     <>
       <div className="font">
@@ -21,67 +86,12 @@ export default function MapScreen() {
           <div className="orange d-none d-md-block" style={{ height: "0.5rem" }} />
         </div>
         <div style={{ display: 'flex', height: '100vh', width: '100%' }}>
-          {/* Menú*/}
-          <div style={{
-            width: '250px',
-            backgroundColor: '#f8f9fa',
-            padding: '20px',
-            borderRight: '1px solid #dee2e6',
-            boxShadow: '2px 0 5px rgba(0,0,0,0.1)'
-          }}>
-            <h2 style={{ marginTop: 0, marginBottom: '20px', color: '#333' }}>Opciones del Mapa</h2>
-
-            {/* Filtros */}
-            <div style={{ marginBottom: '20px' }}>
-              <h4 style={{ marginBottom: '10px' }}>Filtros</h4>
-              <div style={{ marginBottom: '10px' }}>
-                <ComboBox
-                  title="Nivel Educativo"
-                  options={["Todos", "Prebásica", "Básica I-II Ciclo", "Básica III Ciclo", "Media"]}
-                  value={level}
-                  onChange={setLevel}
-                  >
-                 
-                </ComboBox>
-                <ComboBox
-                  title="Año"
-                  options={years}
-                   value={selectedYear}
-                  onChange={setSelectedYear}>
-                </ComboBox>
-              </div>
-            </div>
-
-            {/* Visualización */}
-            <div style={{ marginBottom: '20px' }}>
-              <h4 style={{ marginBottom: '10px' }}>Visualización</h4>
-              <button style={{
-                width: '100%',
-                padding: '8px',
-                marginBottom: '8px',
-                backgroundColor: '#e9ecef',
-                border: '1px solid #ced4da',
-                borderRadius: '4px',
-                cursor: 'pointer'
-              }}>
-                Reiniciar vista
-              </button>
-              <button style={{
-                width: '100%',
-                padding: '8px',
-                backgroundColor: '#e9ecef',
-                border: '1px solid #ced4da',
-                borderRadius: '4px',
-                cursor: 'pointer'
-              }}>
-                Imprimir Mapa
-              </button>
-            </div>
-          </div>
+          {/*menu */}
+          <MapFilters selectedYear={selectedYear} setSelectedYear={setSelectedYear} level={level} setLevel={setLevel}></MapFilters>
 
           {/* Mapa */}
           <div style={{ flex: 1, position: 'relative' }}>
-            <MainMap title="Tasa de Deserción Escolar en Honduras"  year={selectedYear} level={level}/>
+            <MainMap title="Tasa de Deserción Escolar en Honduras"  departments={filteredDepartments} setDepartments={setFilteredDepartments}/>
           </div>
         </div>
 
