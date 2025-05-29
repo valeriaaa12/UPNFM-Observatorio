79d84938b2074e3d74cefd62fc4d124fb571a3e3
valeriaaa12 <valeriaromero1275@gmail.com>
2025-05-29 00:55:03 -0600

    me dio merge conflict :(

--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -5,15 +5,22 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Dropdown } from 'react-bootstrap';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-
 export default function NavBar() {
-    const { t, i18n } = useTranslation('common');  
+  const { t, i18n } = useTranslation('common');
+  const [isClient, setIsClient] = useState(false);
   const [open, setOpen] = useState(false);
   const [open2, setOpen2] = useState(false);
   const [open3, setOpen3] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
   return (
     <>
       <div className='d-none d-lg-block'>
@@ -32,10 +39,7 @@ export default function NavBar() {
             <div className="d-flex align-items-center gap-3 ms-auto whiteText">
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
-                <Nav
-                  className="me-auto my-2 my-lg-0 nav-links-container"
-                  style={{ maxHeight: '100px' }}
-                >
+                <Nav className="me-auto my-2 my-lg-0 nav-links-container" style={{ maxHeight: '100px' }}>
                   <Nav.Link className=" whiteText" href="/landingpage"><b>{t('Home')}</b></Nav.Link>
                   <Nav.Link className=" whiteText" href="/aboutUs"><b>{t("About Us")}</b></Nav.Link>
                   <Nav.Link className=" whiteText" href="#action2"><b>{t("IndicadoresEducativos")}</b></Nav.Link>
@@ -43,18 +47,18 @@ export default function NavBar() {
 
                   {/* Dropdowns */}
                   <NavDropdown
-                    title={<span className="whiteText fw-bold">{t("IndicadoresEducativos")}</span>}
+                    title={<span className="whiteText fw-bold">Indicadores Educativos</span>}
                     id="navbarScrollingDropdown"
                     show={open3}
                     onMouseEnter={() => setOpen3(true)}
                     onMouseLeave={() => setOpen3(false)}
                     className="dropdown-with-fade"
                   >
-                    <NavDropdown.Item href="/Boletines">Cobertura</NavDropdown.Item>
+                    <NavDropdown.Item href="/Boletines">{t("Cobertura")}</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="/mapscreen">Desercion</NavDropdown.Item>
+                    <NavDropdown.Item href="/mapscreen">{t("Desercion")}</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item onClick={() => i18n.changeLanguage('es')}>Repitencia</NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => i18n.changeLanguage('es')}>{t("Repitencia")}</NavDropdown.Item>
                   </NavDropdown>
 
                   <NavDropdown
@@ -65,18 +69,18 @@ export default function NavBar() {
                     onMouseLeave={() => setOpen(false)}
                     className="dropdown-with-fade"
                   >
-                    <NavDropdown.Item href="/Boletines">Boletines</NavDropdown.Item>
+                    <NavDropdown.Item href="/Boletines">{t("Boletines")}</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item onClick={() => i18n.changeLanguage('en')}>Artículos de Interés</NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => i18n.changeLanguage('en')}>{t("Artículos")}</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item onClick={() => i18n.changeLanguage('es')}>Datos Municipales</NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => i18n.changeLanguage('es')}>{t("DatosMunicipales")}</NavDropdown.Item>
                   </NavDropdown>
 
                   <NavDropdown
                     title={<span className="whiteText fw-bold">{t("Comunidad")}</span>}
                     id="navbarScrollingDropdown"
                     show={open2}
-                    align="end" 
+                    align="end"
                     onMouseEnter={() => setOpen2(true)}
                     onMouseLeave={() => setOpen2(false)}
                     className="dropdown-with-fade"
@@ -85,37 +89,33 @@ export default function NavBar() {
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="/login">{t("ExplorarInvestigaciones")}</NavDropdown.Item>
                   </NavDropdown>
-
-                  {/*Fin dropdowns */}
+                  {/* Fin dropdowns */}
                 </Nav>
-                
-                              <Dropdown className="d-inline mx-2 no-underline no_underline" align="end">
-                  <Dropdown.Toggle 
-                  id="dropdown-autoclose-true"
-                  className="p-0 border-0 bg-transparent userMenu" 
-                  as="div"
-                
-                >
-                  <img
-                    src="/images/usuario.png"
-                    width="50px"
-                    height="auto"
-                    className="d-inline-block align-top me-3"
-                    alt="User menu"
-                  />
-                </Dropdown.Toggle>
-                
-                <Dropdown.Menu>
-                  
-                  <Dropdown.Item href="/login">{t("Login")}</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="/register">{t("Register")}</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+
+                <Dropdown className="d-inline mx-2 no-underline no_underline" align="end">
+                  <Dropdown.Toggle
+                    id="dropdown-autoclose-true"
+                    className="p-0 border-0 bg-transparent userMenu"
+                    as="div"
+                  >
+                    <img
+                      src="/images/usuario.png"
+                      width="50px"
+                      height="auto"
+                      className="d-inline-block align-top me-3"
+                      alt="User menu"
+                    />
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="/login">{t("Login")}</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="/register">{t("Register")}</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
               </Navbar.Collapse>
             </div>
           </Container>
-
         </Navbar>
       </div>
     </>
