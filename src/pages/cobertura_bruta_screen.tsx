0ddf40b0368896a3f94e02503498f3eec4d0e7d4
Dariel Sevilla <darielsevilla@gmail.com>
2025-05-31 03:49:02 -0600

    error de idioma corregido

--- src/pages/cobertura_bruta_screen.tsx
+++ src/pages/cobertura_bruta_screen.tsx
@@ -1,8 +1,15 @@
-import MapScreen from "@/screens/mapscreen";
-
+import dynamic from 'next/dynamic';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { GetStaticPropsContext } from 'next';
+import Client from '@/components/client';
+const MapScreen = dynamic(() => import("@/screens/mapscreen"), {
+  ssr: false
+});
 
 export default function CoberturaBrutaScreen(){
     return(<>
+        <Client>
         <MapScreen extensionData="/tasabruta" extensionLimits="/limitesTasaBruta" title="Tasa de Cobertura Bruta Escolar en Honduras"></MapScreen>
+        </Client>
     </>);
 }
--- src/pages/cobertura_neta_screen.tsx
+++ src/pages/cobertura_neta_screen.tsx
@@ -1,8 +1,10 @@
 import MapScreen from "@/screens/mapscreen";
-
-
+import Client from "@/components/client";
 export default function CoberturaNetaScreen(){
     return(<>
+        <Client>
         <MapScreen extensionData="/tasaneta" extensionLimits="/limitesTasaNeta" title="Tasa de Cobertura Neta Escolar en Honduras"></MapScreen>
+        </Client>
     </>);
 }
+
