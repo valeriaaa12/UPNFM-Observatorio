2af99b4aa86f0cd04a741395ffe7432eaafdcaeb
rodrigoAVC <rodrigovasquezcabezas@gmail.com>
2025-06-01 00:25:44 -0600

    Ya admin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,22 @@ import "@/styles/styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from "next/app";
 import { I18nextProvider } from 'react-i18next';
-import { CookiesProvider } from 'react-cookie'; 
+import { CookiesProvider } from 'react-cookie';
+import { UserProvider } from './usertype';
 import i18n from "@/translations/i18n";
 import { appWithTranslation } from 'next-i18next';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <CookiesProvider> 
+    <CookiesProvider>
       <I18nextProvider i18n={i18n}>
-        <Component {...pageProps} />
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
       </I18nextProvider>
     </CookiesProvider>
   );
 }
 
 export default appWithTranslation(App);
+
