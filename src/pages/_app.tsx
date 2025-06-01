576dbbff2e5913e5fc439656d01a2f666387b31f
rodrigoAVC <rodrigovasquezcabezas@gmail.com>
2025-06-01 06:37:32 -0600

    insertarExcel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import "@/styles/styles.css";
+import '@/styles/uploadExcel.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import type { AppProps } from "next/app";
 import { I18nextProvider } from 'react-i18next';
 import { CookiesProvider } from 'react-cookie';
-import { UserProvider } from './usertype';
+import { UserProvider } from '../context/usertype';
 import i18n from "@/translations/i18n";
 import { appWithTranslation } from 'next-i18next';
 
